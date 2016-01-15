2d3437a8dc1dff461b92cff8cf7e4303f7480952
Keith Suderman <suderman@cs.vassar.edu>
2016-01-15 15:17:57 -0500

    Fixed prefix for the PGP plugin.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ wget http://www.lappsgrid.org/keys/secring.gpg.enc
 wget http://www.lappsgrid.org/keys/pubring.gpg.enc
 openssl aes-256-cbc -d -pass pass:"$ENCRYPTION_PASSWORD" -in secring.gpg.enc -out secring.gpg
 openssl aes-256-cbc -d -pass pass:"$ENCRYPTION_PASSWORD" -in pubring.gpg.enc -out pubring.gpg
-mvn -DskipTests=true -P release package javadoc:jar source:jar mvn:sign deploy -Dgpg.passphrase="$PGP_PASSPHRASE" --settings settings.xml
+mvn -DskipTests=true -P release package javadoc:jar source:jar gpg:sign deploy -Dgpg.passphrase="$PGP_PASSPHRASE" --settings settings.xml
 mvn site --settings settings.xml
