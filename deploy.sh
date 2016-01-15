1ef8758bcf3d61369c20512a5881e1e6bbf59d1e
Keith Suderman <suderman@cs.vassar.edu>
2016-01-15 16:24:27 -0500

    Testing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 wget http://www.lappsgrid.org/keys/secring.gpg.enc
 wget http://www.lappsgrid.org/keys/pubring.gpg.enc
+echo "Contents of the .gnupg directory:"
+ls ~/.gnupg
 openssl aes-256-cbc -d -pass pass:"$ENCRYPTION_PASSWORD" -in secring.gpg.enc -out ~/.gnupg/secring.gpg
 openssl aes-256-cbc -d -pass pass:"$ENCRYPTION_PASSWORD" -in pubring.gpg.enc -out ~/.gnupg/pubring.gpg
+echo "Contents of the .gnupg directory:"
 ls ~/.gnupg
 #mvn -DskipTests=true -P release package javadoc:jar source:jar gpg:sign deploy -Dgpg.passphrase="$PGP_PASSPHRASE" --settings settings.xml
 mvn -DskipTests=true -P release package gpg:sign deploy -Dgpg.passphrase="$PGP_PASSPHRASE" --settings settings.xml
